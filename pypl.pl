94ab59148ede6c1c3b631d3d030505bad689af6f
kawing-ho <gavin_kw@hotmail.com>
2017-09-18 18:25:52 +1000

    Coding, minor work done for subset 1

--- pypl.pl
+++ pypl.pl
@@ -4,15 +4,11 @@
 # written by Ka Wing Ho z5087077 18th September 2017
 
 while ($line = <>) {
-    if ($line =~ /^#!/ && $. == 1) {
-
-        # translate #! line
+    if ($line =~ /^#!/ && $. == 1) {   # translate #! line
 
         print "#!/usr/bin/perl -w\n";
 
-    } elsif ($line =~ /^\s*(#|$)/) {
-
-        # Blank & comment lines can be passed unchanged
+    } elsif ($line =~ /^\s*(#|$)/) {   # Blank & comment lines can be passed unchanged
 
         print $line;
 
@@ -21,8 +17,15 @@ while ($line = <>) {
         # Python's print outputs a new-line character by default
         # so we need to add it explicitly to the Perl print statement
 
-        print "print \"$1\\n\";\n";
-
+        #print "print \"$1\\n;\"."\n";
+    } elsif ($line =~ /(\w+) ?= ?([\w _*\/\+\"\'\\-]+)/) {	#variables
+    
+    	  print "\$$1 = $2;\n"   #add support for inline comments later
+    	  
+    } elsif ($line =~ /^\s*print\(([^\)]*)\)$/) {   #print variables eg. print "$1\n";
+    
+    	  #print "print \$$1\n;"."\n";
+    	 
     } else {
 
         # Lines we can't translate are turned into comments
