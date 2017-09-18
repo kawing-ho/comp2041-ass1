13f6bbba2c2d2fcb5aabf42f064fbd6ee975a78e
kawing-ho <gavin_kw@hotmail.com>
2017-09-18 14:52:53 +1000

    Removed andrew's comment lol

--- pypl.pl
+++ pypl.pl
@@ -1,8 +1,7 @@
 #!/usr/bin/perl -w
 
-# Starting point for COMP[29]041 assignment 1
-# http://www.cse.unsw.edu.au/~cs2041/assignments/pypl
-# written by andrewt@unsw.edu.au September 2017
+# COMP2041 assignment 1:  http://www.cse.unsw.edu.au/~cs2041/assignments/pypl
+# written by Ka Wing Ho z5087077 18th September 2017
 
 while ($line = <>) {
     if ($line =~ /^#!/ && $. == 1) {
