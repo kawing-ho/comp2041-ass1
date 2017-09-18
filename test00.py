103f49bb8e9617805ea289acfffb9d864b2873c8
kawing-ho <gavin_kw@hotmail.com>
2017-09-18 17:19:33 +1000

    changes to test00.py

--- test00.py
+++ test00.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-# put your test script here
+# python that has uncinventional style
+# Also the use of "#" character in function calls to mimic comments
+
+import sys
+count = 0
+for line in sys.stdin: sys.stdout.write("Line #{}: {}".format(count,line)); count = count+1;
+
+print(count, "lines in total :)")
+
