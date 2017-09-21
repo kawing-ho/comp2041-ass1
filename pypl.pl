3cbdd6a9a91877a7363eceeb7298eaaa842073b9
kawing-ho <gavin_kw@hotmail.com>
2017-09-21 14:43:13 +1000

    Added test03.py; did some more work for math in python but it's hard ...

--- pypl.pl
+++ pypl.pl
@@ -3,33 +3,72 @@
 # COMP2041 assignment 1:  http://www.cse.unsw.edu.au/~cs2041/assignments/pypl
 # written by Ka Wing Ho z5087077 18th September 2017
 
+our @variables = ();
+
+#All this work just for adding '$'
+sub addDollar {
+	my $in = shift(@_);
+	
+	my @buff = split(" ",$in);   
+	foreach $item (@buff) {
+		foreach $var (@variables) {
+			next if $item =~ /^\$.*/;
+			$item =~ s/$var/\$$var/g;
+		}
+	}
+	return join(" ",@buff);
+}
+
+
 while ($line = <>) {
-    if ($line =~ /^#!/ && $. == 1) {   # translate #! line
 
-        print "#!/usr/bin/perl -w\n";
+	 # translate #! line
+    if ($line =~ /^#!/ && $. == 1) { print "#!/usr/bin/perl -w\n";
+	
+	 # Blank & comment lines can be passed unchanged
+    } elsif ($line =~ /^\s*(#|$)/) { print $line;
 
-    } elsif ($line =~ /^\s*(#|$)/) {   # Blank & comment lines can be passed unchanged
-
-        print $line;
-
-    } elsif ($line =~ /^\s*print\("(.*)"\)$/) {
-
-        # Python's print outputs a new-line character by default
-        # so we need to add it explicitly to the Perl print statement
-
-        #print "print \"$1\\n;\"."\n";
-    } elsif ($line =~ /(\w+) ?= ?([\w _*\/\+\"\'\\-]+)/) {	#variables
-    
-    	  print "\$$1 = $2;\n"   #add support for inline comments later
+	 # print(...) statements
+    } elsif ($line =~ /(\s*)print\(([\"\']?[^\)\'\"]+[\"\']?)\)/) {
     	  
-    } elsif ($line =~ /^\s*print\(([^\)]*)\)$/) {   #print variables eg. print "$1\n";
+    	  #var substitution
+    	  $printz = addDollar($2);
+    	  $space  = $1;
+    	  
+    	  #doing math in printz (no quotes and contains math operators)
+    	  if($printz !~ /^[\"\'][^\'\"]*[\'\"]$/ && $printz =~/[\*\+\-\/\%]+/) {
+#    	  		$operator = $2;
+#    	  		print "operator is $operator\n";
+#    	  		if($operator eq "+") {
+#    	  			$printz = $1 + $3;
+#    	  		} elsif ( $operator eq "*") {
+#    	  			$printz = $1 * $3;
+#    	  		} elsif ($operator eq "-") {   #don't forget exponentiation
+#    	  			$printz = $1 - $3;
+#    	  		} elsif ($operator eq "/") {
+#    	  			$printz = $1 / $3;
+#    	  		}
+    	  }
+    	  
+        print "$space"."print"." \""."$printz"."\\n\";\n";
+        
+    #variables
+    } elsif ($line =~ /(\s*)([\w]+)\s*=\s*([\w _*\/\+\"\'\\-]+)/) {
+    	  push @variables, $2;  #Add variables to list
+    	  $t = addDollar($3);
+    	  print "$1"."\$"."$2 = $t;"."\n";
+    	  
+    #if statements
+    } elsif ($line =~ /^\s*print\(([^\)]*)\)$/) { next;
+    
+    #while loops
+    #} elsif (1 eq 1){ next;
+    
+    #for loops
+    #} elsif (0 eq 0){ next;
     
     	  #print "print \$$1\n;"."\n";
     	 
-    } else {
-
-        # Lines we can't translate are turned into comments
-
-        print "#$line\n";
-    }
+    # Lines we can't translate are turned into comments
+    } else { print "#$line\n"; }
 }
--- test02.py
+++ test02.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-# put your test script here
+# complex math in VARIABLES ??? 
+
+a = 1 + (2 + 3) + 4 * 5
+c = (0**100/10)**20
+b = a + c + (c + c**2) + (a*0)
+
+print (b)
