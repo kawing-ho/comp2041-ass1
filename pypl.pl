131ee024353efa406220ade8716d37a519c7e787
kawing-ho <gavin_kw@hotmail.com>
2017-10-05 00:42:36 +1100

    Final commit before submission

--- pypl.pl
+++ pypl.pl
@@ -5,20 +5,16 @@
 
 use Text::Tabs;
 
-#Todo / Haven't supported
-# - bitwise operators: | ^ & << >> ~
-
-
 our %variables = ();
 our $closingExpected = 0;
 our $previousIndent = "";
 my $previousIf = "";
 
-#finding variables and adding '$' to them
-#ignore things in quotes
-#find arrays and adding '@' to them
-sub process {
+#finding variables and adding '$' to them (ignore things in quotes)
+sub findVariables {
 	my $in = shift(@_);
+	
+	#flag for checking if a quote has currently been opened/closed
 	my $QUOTE = 0;
 	
 	my @buff = split(" ",$in);
@@ -26,19 +22,17 @@ sub process {
 		
 		#if quote detected toggle on/off
 		if ($item =~ /[\'\"]/) {
-			next if $item =~ /^[\'\"].*?[\'\"]$/;
-			#print "#$item matched quote\n";
+			next if $item =~ /^[\'\"].*?[\'\"]$/;  #skip if quotes appear twice (double negative)
 			if($QUOTE == 0) { $QUOTE = 1; } else { $QUOTE = 0; }
 			next;
 		}
 		
-		#if($QUOTE == 1) { print "skipping $item \n";}
-		next if($QUOTE == 1);
+		next if($QUOTE == 1); #skip any variables found within quotes
 		
-		foreach $var (keys %variables) {
-			next if $item =~ /^\$.*/;
-			next if $item =~ /[a-zA-Z]$var/;
-			next if $item =~ /$var[a-zA-Z]/;
+		foreach $var (keys %variables) {    #do not add $ to an item if
+			next if $item =~ /^\$.*/;        # if already has a $ infront
+			next if $item =~ /[a-zA-Z]$var/; # if the var is connected to another word
+			next if $item =~ /$var[a-zA-Z]/; # eg. $a -> and / banana  ($and b$an$an$a is wrong)
 			$item =~ s/$var/\$$var/g;
 		}
 	}
@@ -49,9 +43,6 @@ sub process {
 		my $arg = $1;
 		my $rep = "int($arg)";
 		my $name = $arg; $name =~ s/^\$//;
-		#print "#\$in = $in, \$arg = $arg with value $variables{$name}\n";
-		#replace variable with int version as well
-		$variables{$name} = int $variables{$name} if(defined $variables{$name});
 		
 		$in =~ s/\Q$rep\E/int $arg/g;
 		
@@ -61,13 +52,14 @@ sub process {
 }
 
 #Look for print statements in a string and FORMAT them !
+#only useful if bad style is used eg. if( a > 1): print(a); print(b)
 sub formatPrint {
 	my $in = shift(@_);
 	my $out = "";
 	my @buff = split ';' , $in;
 	
 	foreach $i (@buff) {
-		#print "#format print: $i\n";
+
 		#format if its print statement
 		if($i =~ /print\s*\((.*)\)/) {
 			my $t = $1;
@@ -75,14 +67,16 @@ sub formatPrint {
 			$i = "print \"$t\\n\""
 		}
 		
+		#semicolon check 
 		if($out eq "") { $out = $i; } else { $out = $out.";".$i; }
-		#print "#\$out = $out\n";
+
 	}
 	
    return $out;
 }
 
-#adds extra brackets if number of brackets in a line are mismatched
+#adds extra brackets ( ( ) ) if number of brackets in a line are mismatched
+#could've used a stack but its too late now
 sub checkBrace {
 	my $in = shift(@_);
 	my $openCount = 0;
@@ -94,6 +88,7 @@ sub checkBrace {
 		$closeCount++ if ($char eq ")");
 	}
 	
+	#DO replacing for brackets if the counts do not match
 	if($openCount != $closeCount) {
 		while ($openCount < $closeCount) {
 			$in = "(".$in;
@@ -115,8 +110,9 @@ sub checkBrace {
 sub checkCondition {
 	my $in = shift(@_);
 	
-	#print "# \$in = $in\n";
-	#if u have any operators followed by "/' substitute !
+	#this wall of regex basically says:
+	#if you have an operator followed by a open/close quote 
+	#its most likely for a string, so change == to eq , != to ne
 	$in =~ s/\s*==\s*\"/ eq \"/g; $in =~ s/\"\s*==\s*/\" eq /g;
 	$in =~ s/\s*==\s*\'/ eq \'/g; $in =~ s/\'\s*==\s*/\' eq /g;
 	$in =~ s/\s*!=\s*\"/ ne \"/g; $in =~ s/\"\s*!=\s*/\" ne /g;
@@ -124,42 +120,41 @@ sub checkCondition {
 	
 	#if comparing between variable check that the variable stores a string
 	my @buff = split(/[\&\|]{2}/,$in);
-	foreach $check (@buff) {  #check example $a == $b
-		my ($var1, $op, $var2) = $check =~ /\$(\w+)\s*([!=]{2})\s*\$(\w+)/ or next;
-		#print "### \$v1:$var1//$variables{$var1}, \$op:$op, \$v2:$var2//$variables{$var2}\n";
+	foreach $check (@buff) {  #for example $a == $b where a and b are both strings
+		my ($var1, $op, $var2) = $check =~ /\$(\w+)\s*([!=]{2})\s*\$(\w+)/ or next;  #skip if no match
 		
 		#if either var1 or var2 is a string var have to change
 		if($variables{$var1} =~ /[\"\']/ || $variables{$var2} =~ /[\"\']/) {
-			#print "#should be replacing for $check\n";
+
 			my $check2 = $check;
 			if($op =~ m/==/) { $check2 =~ s/==/eq/; }
 			if($op =~ m/!=/) { $check2 =~ s/!=/ne/; }
-			#print "#\$in = $in\n";
+
 			$in =~ s/\Q$check\E/$check2/;
-			#print "#\$out = $in\n";
 		}
 	}
 	return $in;
 }
 
+#looks at indent and decides whether a bracket needs to be added or not
 sub checkIndent {
 	my $in = shift(@_);
-	#print "#\$closingExpected = $closingExpected\n";
 
-	#check for indentation
+	#check for indentation + length of indent
 	my ($indent) = $in =~ /^(\s*)/;
 	my $li = length $indent;
-	 
+	
 	if($previousIndent ne "") {
 		$pli = length $previousIndent;
-		#print "#\$li = $li, \$pli = $pli\n";
 	 	
 		#previous indent 
 		if($li < $pli && $closingExpected > 0) {
 		
 			while($li < $pli) {
-				#print "#\$pli = $pli, doing something\n";
+
 				$closingIndent = $previousIndent;
+				
+				#very brute force way of "removing indents for next line"
 				if($closingIndent =~ /^ /) {    #add support for tabs/4space/3space later
 					$closingIndent =~ s/^ {4}//;
 				} elsif ($closingIndent =~ /^\t/) { 
@@ -168,7 +163,10 @@ sub checkIndent {
 				
 				$previousIndent = $closingIndent;
 				$pli = length $previousIndent;
-				if($pli != $li || $line !~ /^\s*elif|else/) {
+				if($pli != $li || $line !~ /^\s*elif|else/) {  
+				   #prints a { if the line read from stdin isn't elif/else
+				   #since these statements come with their own close brace (eg. } else { )
+				   #so printing another brace would be extra 
 					print "$closingIndent}\n";
 					#$closingExpected--;
 				}
@@ -183,49 +181,53 @@ sub checkIndent {
 	$previousIndent = $indent if($line !~ /^\s*$/);
 }
 
-while ($line = <>) {
 
-	 #print "#processing $line";
+
+
+
+while ($line = <>) {
 
 	 #skip comment and blank lines after first hashbang line
 	 print "$line" and next if($line =~ /^\s*(#|$)/ && $. != 1);
 	 
-	 #check Indentation at the start of line 
+	 #check Indentation/brackets at the start of line 
 	 checkIndent($line);
 	
 	 #comment out import statements
 	 print "#".$line and next if $line =~/^import/;
 	 
-	 #change all instances of readline to STDIN
+	 #change all instances of readline to STDIN (yes its not the most stable way to do it I know)
 	 $line =~ s/sys.stdin.readline\(\)/<STDIN>/g;
 
 	 # translate #! line
     if ($line =~ /^#!/ && $. == 1) { print "#!/usr/bin/perl -w\n";
 	 
-	 # print(...) statements / sys.stdout.write statements  #[^\)]
+	 # =====print(...) statements / sys.stdout.write statements=====
+	 #regex says: if the line matches print/sys.stdout.write with ( ... ) and optional comment
+	 #this regex is pretty much the same for all others below
     } elsif ($line =~ /^(\s*)(print|sys.stdout.write)\s*\(([\"\']?.*[\"\']?)\)\s*(#.*)*$/) {
     	  
     	  #var substitution
     	  $space  = $1;
     	  $stdout = $2;
-		  $printz = process($3);
+		  $printz = findVariables($3);
 		  $comment = $4 || "";
     	  
-    	  #doing math in printz (no quotes and contains math operators)
+    	  #doing math in printz (regex: no quotes and contains math operators)
     	  if($printz !~ /^[\"\'][^\'\"]*[\'\"]$/ && $printz =~/[\*\+\-\/\%]+/) {
     	  		
     	  		#interpolate the values of variables into the printz string
+    	  		#eg: print( $a + $b) -> print( 1 + 2)
     	  		foreach $var (keys %variables) {
     	  			next if $printz !~ m/\$$var/;
     	  			$value = $variables{$var};
     	  			$replace = "$value";
-    	  			print "#$var has value $value\n";
     	  			$printz =~ s/(\$$var)/$replace/g;
     	  		}
     	  		
     	  		#substitue // for /
     	  		$printz =~ s/\/\//\//g;
-    	  		$printz = eval $printz;
+    	  		$printz = eval $printz;  #math magic done here
     	  }
     	  
     	  #remove extra quotes
@@ -239,12 +241,18 @@ while ($line = <>) {
     	  		print "$space"."print"." \""."$printz"."\"; $comment\n";
     	  }
     
-    #else statements
+    #=====else statements======
     } elsif ($line =~ /^(\s*)else\s*:\s*(.*?)\s*(#.*)*$/) {
     	$space = $1;
-    	$statement = process($2);
+    	$statement = findVariables($2);
     	$comment = $3 || "";
     	
+    	
+    	#if the previous if statement already closed the brace on the same line then
+    	#the current line doesn't need to have one as well 
+    	#eg. if( a == b) { ... }    VS     if( a == b) { 
+      #												  . . .
+    	#eg. else { ... }                  } else { ... }
     	if(!defined $statement || $statement eq "") {   #on different line
     		$closingExpected++;
     		if($previousIf =~ /\}\s*$/) { print "$space"."else "."\{ $comment\n";
@@ -255,8 +263,7 @@ while ($line = <>) {
     	 	} else { print "$space"."} else "."\{ "."$statement\;"." \} $comment\n"; $closingExpected--;}
     	 }
         
-    #if/elif statements
-    #need to support logical operators as well
+    #=====if/elif statements=====
     } elsif ($line =~ /^(\s*)(if|elif)\(?([^\:]+)\)?:\s*(.*?)\s*(#.*)*$/) {
     	 $space = $1;
     	 $if = $2;
@@ -266,13 +273,14 @@ while ($line = <>) {
     	 	if($previousIf =~ /\}\s*$/) { $if = "elsif" } else { $if = "} elsif"; }
     	 }
     	 
-    	 $condition = process($3);
-    	 $statement = process($4);
+    	 $condition = findVariables($3);
+    	 $statement = findVariables($4);
     	 
     	 $condition =~ s/and/\&\&/g; $condition =~ s/or/\|\|/g; $condition =~ s/not/\!/g;
     	 $condition = checkCondition($condition);
     	 $condition = checkBrace($condition); 
     	 
+    	 #same case as the "else" statement
     	 if(!defined $statement || $statement eq "") {   #on different line
     	 	print "$space"."$if($condition) "."\{ $comment\n";
     	 	#if($if eq "if") {$closingExpected++;}
@@ -288,11 +296,10 @@ while ($line = <>) {
     	 	$previousIf = "$space"."$if($condition) "."\{ "."$statement\;"." \} $comment\n";
     	 }
     
-    #while loop
-    #need to support logical operators as well
+    #=====while loop=====
     } elsif ($line =~ /^(\s*)while\s*\(?([^\:]*)\)?:\s*(.*?)\s*(#.*)*$/) {
-    	 $condition = process($2);
-    	 $statement = process($3);
+    	 $condition = findVariables($2);
+    	 $statement = findVariables($3);
     	 $comment = $4 || "";
     	 $condition =~ s/and/\&\&/g; $condition =~ s/or/\|\|/g; $condition =~ s/not/\!/g;
     	 $condition = checkCondition($condition);
@@ -307,31 +314,15 @@ while ($line = <>) {
     	 }
     
     
-    #variables
-    #needs to be upgraded to handle math operations in variables as well
-    #needs to be upgraded to handle other variables as well ...
-    #\s*([\w \_\*\/\+\%\"\'\\\(\)\<\>-]+)
+    #=====variables=====
     } elsif ($line =~ /(\s*)([\w]+)\s*=\s*(.*?)\s*(#.*)*$/) {
     	  $space = $1;
     	  $varname = $2;
     	  $comment = $4 || "";
-    	  $t = process($3);
+    	  $t = findVariables($3);
+    	  
+    	  #if the RHS isn't a string (no quotes which implies its math) replace // with /
     	  if($t !~ /^[\"\'][^\'\"]*[\'\"]$/) {$t =~ s/\/\//\//g;}
-    	  #doing math in variables (no quotes and contains math operators)
-#    	  if($t !~ /^[\"\'][^\'\"]*[\'\"]$/ && $t =~/[\*\+\-\/\%]+/) {
-    	  		
-    	  		#interpolate the values of variables into the printz string
-#    	  		foreach $var (keys %variables) {
-#    	  			next if $t !~ m/\$$var/;
-#    	  			$value = $variables{$var};
-#    	  			$replace = "$value";
-#    	  			$t =~ s/(\$$var)/$replace/g;
-#    	  		}
-    	  		
-    	  		#substitue // for /
-#    	  		$t =~ s/\/\//\//g;
-#    	  		$t = eval $t;
-#    	  }
     	  
     	  #special treatment for int( $xxx )
     	  if($t =~ /int\s*\$(\w+)/) { $t = int $variables{$1};}
@@ -340,23 +331,22 @@ while ($line = <>) {
     	  $variables{$varname} = $t;  #Hash variable to values
     	  print "$space"."$declare\$"."$varname = $t; $comment"."\n";
      
-    #for loops
+    #=====for loops=====
     } elsif ($line =~ /^(\s*)for\s*(\w+)\s*in\s*(.*?):\s*(.*?)\s*(#.*)*$/) {
     	$space = $1;
     	$loop  = $2;
     	$iterable = $3;
     	$comment = $5 || "";
     	$variables{$loop} = 0;  #add to list of variables
-    	$loop = process($loop);
-    	$statement = process($4);
+    	$loop = findVariables($loop);
+    	$statement = findVariables($4);
     	
     	#$i in range
     	if($iterable =~ /range/) {
-    		#find out which type of range it is
+    		#find out which type of range it is  -> regex to catch range(x) , range(x,x) , range(x,x,x)
     		($start,$stop,$step) = $iterable =~ /range\(\s*([^\,]+)\s*,?\s*([^\,]+)?\s*,?\s*([^\)]*)?\s*\)/;
     		
-    		
-    		$start = process($start); $stop = process($stop); $step = process($step);
+    		$start = findVariables($start); $stop = findVariables($stop); $step = findVariables($step);
     		#print "\$start = $start, \$stop = $stop, \$step = $step\n";
     		
     		if(defined $start && !defined $stop && (!defined $step || $step eq "")) {
@@ -380,9 +370,9 @@ while ($line = <>) {
     		}
     		
     	
-    	} #else if its an array/list 
+    	} #else if its an array/list (didn't reach subset4 unfortunately)
     	
-    #continue and break
+    #=====continue and break=====
     } elsif ($line =~ /continue|break/ && $line !~ /^\#/) {
     		$line =~ s/continue/next\;/g;
     		$line =~ s/break/last\;/g;
@@ -393,11 +383,13 @@ while ($line = <>) {
     
 }
 
-#if no more STDIN but closing brace still expected add one !
+#if no more STDIN but closing brace still expected add them !
+#special case where the last few lines of the program are if statement
 while ($closingExpected > 0) {
 	#print "#$previousIndent/previousIndent\n";
 	$closingIndent = $previousIndent;
 	
+	#brute force removal of indent again
 	if($closingIndent =~ /^ /) {    $closingIndent =~ s/^ {4}//;
 	} elsif ($closingIndent =~ /^\t/) { $closingIndent =~ s/^\t//;
 	} else { print "#error in indentations!\n";}
